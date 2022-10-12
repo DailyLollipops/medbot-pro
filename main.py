86ac361da63ff52d3ea1137e8e04eee2348dca09
DailyLollipops <clarencemadrigal84@gmail.com>
2022-10-12 23:58:59 +0800

    Changed database settings

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from database import Database
 from medbot import Medbot
 
 if __name__ == "__main__":
-    database = Database('localhost','medbot','clarence','admin')
+    database = Database('sql.freedb.tech','freedb_medbot','freedb_medbot','ct9xVSS$$2g35s7')
     medbot = Medbot(database)
     user = medbot.login()
     print(user.get_info())
