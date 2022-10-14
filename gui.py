14cafb32daab599ee06bb389944e56c7aed49c87
DailyLollipops <clarencemadrigal84@gmail.com>
2022-10-14 18:16:33 +0800

    Changed window size to match full screen

--- gui.py
+++ gui.py
@@ -18,24 +18,24 @@ class MedbotLoginGui:
     def __init__(self, medbot):
         self.window = tkinter.Tk()
         self.window.title('Login Window')
-        self.window.geometry('600x400')
+        self.window.geometry('800x440')
         logo = ImageTk.PhotoImage(Image.open('images/logo.png'))
         self.window.iconphoto(False, logo)
         self.window.configure(background = 'white')
         self.medbot = medbot
  
-        placeholder = tkinter.Canvas(self.window, width = 280, height = 390)
+        placeholder = tkinter.Canvas(self.window, width = 380, height = 400)
         qrcode = ImageTk.PhotoImage(Image.open('images/qrcode.png').resize((128,166)))
-        placeholder.create_text(145, 65, text = 'Med-bot: Pulse Rate\nand\nBlood Pressure Monitor',
+        placeholder.create_text(190, 65, text = 'Med-bot: Pulse Rate\nand\nBlood Pressure Monitor',
                                 anchor = tkinter.CENTER, font = ('Lucida',14), justify = 'center')
-        placeholder.create_image(85, 130, image = qrcode, anchor = tkinter.NW)
-        placeholder.create_text(150, 355, text = 'Place your QR Code\nwithin the frame to Login',
+        placeholder.create_image(125, 130, image = qrcode, anchor = tkinter.NW)
+        placeholder.create_text(180, 355, text = 'Place your QR Code\nwithin the frame to Login',
                                 anchor = tkinter.CENTER, font = ('Lucida',14), justify = 'center')
         placeholder.configure(background = 'white', highlightbackground = 'white' )
         placeholder.place(x = 0, y = 0)
 
-        self.canvas = tkinter.Canvas(self.window, width = 300, height = 380)
-        self.canvas.place(x = 290, y = 10)
+        self.qrcode_scanner_frame = tkinter.Canvas(self.window, width = 400, height = 400)
+        self.qrcode_scanner_frame.place(x = 390, y = 10)
         self.update()
         self.window.mainloop()
 
@@ -44,7 +44,7 @@ class MedbotLoginGui:
             ret, frame = self.medbot.get_qrcode_scanner_frame()
             if ret:
                 self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
-                self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+                self.qrcode_scanner_frame.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
                 try:
                     logged_in = self.medbot.login_tk(frame)
                 except:
@@ -61,6 +61,11 @@ class MedbotMainWindow():
         self.root = root
         self.root.window.withdraw()
         self.window = tkinter.Toplevel(self.root.window)
+        self.window.title('Login Window')
+        self.window.geometry('800x440')
+        logo = ImageTk.PhotoImage(Image.open('images/logo.png'))
+        self.window.iconphoto(False, logo)
+        self.window.configure(background = 'white')
         self.window.protocol("WM_DELETE_WINDOW", self.on_close)
         self.update()
 
