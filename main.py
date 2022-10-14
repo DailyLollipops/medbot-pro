7e4d14f0019d0975041c72e64e03736214309779
DailyLollipops <clarencemadrigal84@gmail.com>
2022-10-14 22:08:27 +0800

    Just some test on threading

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from database import Database
 from medbot import Medbot
-
+from threading import Thread
 if __name__ == "__main__":
     database = Database('sql.freedb.tech','freedb_medbot','freedb_medbot','ct9xVSS$$2g35s7')
     medbot = Medbot(database)
     user = medbot.login()
     print(user.get_info())
+    medbot.set_speaker_properties(voice = 'jarvis')
+    text = 'Please position your arm correctly'
+    test = Thread(target=lambda:medbot.speak('test'))
+    test.start()
     medbot.logout()
     print(medbot.get_current_user())
