086398a59f3bcc0272b6c38972c8bf629d69160a
DailyLollipops <clarencemadrigal84@gmail.com>
2022-10-28 19:00:45 +0800

    additional import checks for oximeter

--- medical_robot/__init__.py
+++ medical_robot/__init__.py
@@ -2,7 +2,7 @@ from .__medbot import *
 from .__database import *
 from .__user import *
 from .__bp3gy12n import *
-# from .__max30102 import *
+from .__max30102 import *
 from .__utility import *
 
 __all__ = [
@@ -10,6 +10,6 @@ __all__ = [
     __database.__all__,
     __user.__all__,
     __bp3gy12n.__all__,
-    # __max30102.__all__,
+    __max30102.__all__,
     __utility.__all__
     ]
